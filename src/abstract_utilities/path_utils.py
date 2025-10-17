b8ad3a5d692ec1485b54c0f615b44629ea8cffb0
AbstractEndeavors <partners@abstractendeavors.com>
2025-10-17 12:40:53 -0500

    Deploy version 0.2.2.438 at 2025-10-17 17:40:53 UTC

--- src/abstract_utilities/path_utils.py
+++ src/abstract_utilities/path_utils.py
@@ -28,7 +28,7 @@ from .string_clean import eatAll
 from .list_utils import make_list
 from .type_utils import get_media_exts, is_media_type,MIME_TYPES
 from .safe_utils import safe_join
-from .class_utils import get_caller_path.get_caller_dir
+from .class_utils import get_caller_path,get_caller_dir
 
 def get_os_info():
     """
