a08dbcccc2c3b16de69a6d6f386ffc4862a60957
putkoff <putkey2@sbclobal.net>
2026-04-12 21:04:33 -0500

    Release abstract_utilities==0.2.2.766 @ 2026-04-13 02:04:33 UTC

--- src/abstract_utilities/hash_utils/imports/imports.py
+++ src/abstract_utilities/hash_utils/imports/imports.py
@@ -3,4 +3,4 @@ from collections import defaultdict
 from ...imports import hashlib
 from ...read_write_utils import safe_dump_to_json
 from ...list_utils import make_list
-
+from ...file_utils import get_files_and_dirs
